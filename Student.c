8ebbfe62dc849494286608f7780567f85e857398
Tin922 <tinjuric92@gmail.com>
2022-10-16 13:28:18 +0200

    student update

--- Student.c
+++ Student.c
@@ -5,6 +5,7 @@
 #include <malloc.h>
 #define MAX_NAME 1024
 #define MAX_SIZE_ROW 1024
+#define MAX_BR_BODOVA 50
 
 typedef struct Student {
 	char Name[MAX_NAME];
@@ -37,17 +38,43 @@ int CountF()
 	return countStudent;
 }
 
+void getDataAndPrintData(int countStudent)
+{
+	Student *s;
+	FILE *f;
+	float relativan_br_bodova=0; //postotak rijesenosti?
+	int br_bodova=0;
+	
+	s=(Student*)malloc(sizeof(Student)*countStudent);
+	
+	f=fopen("studenti.txt","r");
+
+	for(int i=0;i<countStudent;i++)	
+	{
+		fscanf(f,"%s %s %d", s[i].Name,s[i].Surname,&s[i].StudentNumber);
+	}
+
+	for(int i=0;i<countStudent;i++)
+	{
+		relativan_br_bodova=0;
+		printf("%s %s %d\n",s[i].Name,s[i].Surname,s[i].StudentNumber);
+		relativan_br_bodova=s[i].StudentNumber/(float)MAX_BR_BODOVA*100;
+		printf("Realtivan broj bodova je:%f\n",relativan_br_bodova);
+	}
+
+
+}
+
+
+
 void main()
 {
 	int countStudent = 0;
 	char buffer[MAX_SIZE_ROW] = { 0 };
 	FILE* f = NULL;
 	
+	
 	countStudent=CountF();
-
-
+	getDataAndPrintData(countStudent);
 	
-	
-	printf("CountStudent ");
-	printf("CountStudent = %d", countStudent);
 }
